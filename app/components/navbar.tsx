1328ebdd2d15d125932780cf26c9671f0d2b1d7a
GianBkk <gian200308@gmail.com>
2023-04-13 15:12:18 +0200

    change to hero and nav breakpoints

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,15 +7,19 @@ import { useEffect, useState } from 'react'
 
 const sections = [
     {
+        'key': 1,
         'name': 'Home',
         'href': '#herosection'
     },{
+        'key': 2,
         'name': 'About',
         'href': '#aboutsection'
     },{
+        'key': 3,
         'name': 'Projects',
         'href': '#projectssection'
     },{
+        'key': 4,
         'name': 'Contact',
         'href': '#contactsection'
     }
@@ -51,18 +55,20 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
+      console.log(window.scrollY)
+      if(window.scrollY > window.innerHeight){
+        return
+      }
       if (window.scrollY > window.innerHeight / 2) {
         setIsOpen(true)
-      } else {
+        
+      } if(window.scrollY < 5){
         setIsOpen(false)
       }
     };
 
     window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  },[])
+  },[window.innerHeight])
 
   const openNavbar = () => {
     setIsOpen(!isOpen);
@@ -75,7 +81,7 @@ const Navbar = () => {
     <nav className={`${styles.navbar} ${isOpen ? styles.open : styles.closed}`}>
         <svg onClick={openNavbar} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
         {sections.map((item) => (
-          <NavItem name ={item.name} href ={item.href} activeSection={activeSection} />  
+          <NavItem name ={item.name} key={item.key} href ={item.href} activeSection={activeSection} />  
         ))}
     </nav>
   )
--- app/hero/hero.tsx
+++ app/hero/hero.tsx
@@ -6,27 +6,33 @@ import Button from './button'
 const Hero = () => {
   return (
     <section id='herosection' className={styles.main}>
-      <div className={styles.imageOverlay}></div>
-      <Image
-      src="/images/leaf.jpg"
-      alt="My Image"
-      width={500}
-      height={500}
-      className={styles.image}
-      />
-      <Image
-      src="/images/profile.png"
-      alt="My Image"
-      width={500}
-      height={500}
-      className={styles.profile}
-      />
-      <div className={styles.backgroundgradiant}></div>
-      <span className={styles.head}>
-        <h1 >Gian <br /> Bakker</h1>
-        <h2>Full-Stack Developer, Network Engineer</h2>
-        <Button/>
-      </span>
+      
+        <div className={styles.imageOverlay}></div>
+        <Image
+        src="/images/leaf.jpg"
+        alt="My Image"
+        width={500}
+        height={500}
+        className={styles.backgroundImage}
+        />
+
+      <div className={styles.content}>
+        <Image
+        src="/images/profile.png"
+        alt="My Image"
+        width={500}
+        height={500}
+        className={styles.profile}
+        />
+        <span className={styles.textBlock}>
+          <h1>Gian <br/> Bakker</h1>
+          <h3>Full-Stack Developer, Network Engineer</h3>
+          <Button/>
+        </span>
+      </div>
+      
+      
+      
 
       
       
